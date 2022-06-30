eaf32d5a7eaccf5d7a10d80d0a80d901bdfa0307
ciprianciuhan <cipriancucuciuhan@gmail.com>
2022-06-30 17:50:12 +0300

    first commit

---
+++ src/main/java/com/app/homeappraiserv2/HomeAppraiserV2Application.java
@@ -0,0 +1,13 @@
+package com.app.homeappraiserv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAppraiserV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeAppraiserV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/homeappraiserv2/HomeAppraiserV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.homeappraiserv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAppraiserV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
